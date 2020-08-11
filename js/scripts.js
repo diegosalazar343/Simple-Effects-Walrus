9912ca2da1e2bb9a3468fd94e5282abc03358847
diegosalazar343 <salazardiego343@gmail.com>
2020-08-11 10:24:03 -0700

    add text on dog, image of dog, jquerry section on dog
    Co-authored-by: Ophelia Swen <Ophelia.swen@gmail.com>

---
+++ js/scripts.js
@@ -0,0 +1,13 @@
+$(document).ready(function() {
+  $(".clickable").click(function() {
+    $("#walrus-showing").fadeIn();
+    $("#walrus-hidden").fadeIn();
+  });
+});
+
+$(document).ready(function() {
+  $(".click").click(function() {
+    $("#dog-showing").fadeIn();
+    $("#dog-hiding").fadeIn();
+  });
+});
